274bec0c51931f148220c9ec48d1dc22e4e88b9b
Jordan Baumard <jordanbmrd@gmail.com>
2021-12-20 20:41:10 +0100

    ☃️ Some bug fixed + Signature added

---
+++ src/FlagApp.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Game from './Game';
+import './FlagApp.css';
+
+class FlagApp extends Component {
+	render() {
+		return (
+		  <div className="FlagApp">
+			  <div>
+			    <header style={{backgroundImage: 'url("/bg.jpeg")'}}>
+			    	<h1 className="title">Guess The Flag</h1>
+			    </header>
+
+			    <Game className="gameComp" />
+		      </div>
+
+		      <small>Made with <span style={{ color: "red" }}>❤</span> by Jordan</small>
+		  </div>
+		);
+	}
+}
+
+export default FlagApp;
